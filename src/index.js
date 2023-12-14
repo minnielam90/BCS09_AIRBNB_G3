9f5b889270b9e47ca3ef7078a70bc56ca3fadedb
minnielam90 <minnie.lam90@gmail.com>
2023-12-14 10:23:31 +0700

    cmmit lai

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 // redux
 import { Provider } from "react-redux";
